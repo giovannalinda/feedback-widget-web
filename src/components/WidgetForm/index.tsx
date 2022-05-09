99786a647aa2589ca013a8d2c0c3cd9721ba6f86
Giovanna Linda <71105672+giovannalinda@users.noreply.github.com>
2022-05-09 12:21:44 -0300

    adding widget component with tailwind (#1)

---
+++ src/components/WidgetForm/index.tsx
@@ -0,0 +1,69 @@
+import { useState } from "react"
+
+import bugImageUrl from '../../assets/bug.svg'
+import ideaImageUrl from '../../assets/idea.svg'
+import thoughtImageUrl from '../../assets/thought.svg'
+
+import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep"
+import { FeedbackContentStep } from "./Steps/FeedbackContentStep"
+import { FeedbackSucessStep } from "./Steps/FeedbackSucessStep"
+
+export const feedbackTypes = {
+  BUG: {
+    title: "Problema",
+    image: {
+      source: bugImageUrl,
+      alt: 'Imagem de um inseto'
+    }
+  }, 
+  IDEA: {
+    title: "Ideia",
+    image: {
+      source: ideaImageUrl,
+      alt: 'Imagem de uma lâmpada'
+    }
+  },
+  OTHER: {
+    title: "Outro",
+    image: {
+      source: thoughtImageUrl,
+      alt: 'Imagem de um balão de pensamento'
+    }
+  }
+}
+
+export type FeedbackType = keyof typeof feedbackTypes
+
+export function WidgetForm() {
+  const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
+  const [feedbackSent, setFeedbackSent] = useState(false)
+
+  function handleRestartFeedback() {
+    setFeedbackSent(false)
+    setFeedbackType(null)
+  }
+
+  return (
+    <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+      { feedbackSent ? (
+        <FeedbackSucessStep onFeedbackRestartRequested={handleRestartFeedback} />
+      ) : (
+        <>
+          {!feedbackType ? (
+            <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+          ) : (
+            <FeedbackContentStep 
+              feedbackType={feedbackType} 
+              onFeedbackRestartRequested={handleRestartFeedback}
+              onFeedbackSent={() => setFeedbackSent(true)}
+            />
+          )}
+        </>
+      )}
+
+      <footer className="text-xs text-neutral-400">
+        Feito com ♥ pela <a className="underline underline-offset-2" href="https://keepo.io/giovannasouza">Giovanna</a>
+      </footer>
+    </div>
+  )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
 module.exports = {
   content: ["./src/**/*.tsx"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          300: '#996DFF',
+          500: '#8257e6'
+        }
+      },
+      borderRadius: {
+        md: '4px'
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('tailwind-scrollbar'),
+  ],
 }
